8c004232d3b6640c4dba0e7cd4a7bc7af074578e
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-06-05 22:32:51 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,19 @@ import helmet from "helmet"
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT
+
+// Configure CORS options
+const corsOptions = {
+  origin: 'http://localhost:3000', 'https://helplytics-project.vercel.app' // Allow only your frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies/auth headers if needed
+  optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+};
 
 app.use(express.json());
 app.use(helmet())
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Logger
 app.use((req, res, next) => {
